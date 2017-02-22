43337fc6938917b744c1519cd5bc880c2e9db947
Venkatesh Mamidipalli <venki.sree008@gmail.com>
2017-02-22 15:03:25 +0530

    Added routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  selector:'my-app',
+  template: `
+  <h1>{{title}}</h1>
+   <nav>
+     <a routerLink="/dashboard">Dashboard</a>
+     <a routerLink="/heroes">Heroes</a>
+   </nav>
+   <router-outlet></router-outlet>
+  `
 })
-export class AppComponent  { name = 'Venki!!'; }
+
+export class AppComponent{
+  title = 'Tour of Heroes';
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,38 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
+import { HeroesComponent }  from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule , FormsModule, RouterModule.forRoot([
+    {
+      path: '',
+      redirectTo: '/dashboard',
+      pathMatch: 'full'
+    },
+    {
+      path: 'heroes',
+      component: HeroesComponent
+    },
+    {
+      path: 'dashboard',
+      component: DashboardComponent
+    },
+    {
+      path: 'detail/:id',
+      component: HeroDetailComponent
+    }
+  ])],
+  declarations: [ HeroesComponent, HeroDetailComponent, AppComponent ,DashboardComponent],
+  providers: [HeroService],
   bootstrap:    [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+
+}
